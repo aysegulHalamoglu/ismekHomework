6c9196576c7c316a372d363e1116c140075caf2e
Aysegul H <ahalamoglu19@gmail.com>
2022-12-02 00:55:44 +0300

    RecyclerViewAdapter draft

---
+++ app/src/main/java/com/halamoglu/ismekhomework/RecyclerViewAdapter.kt
@@ -0,0 +1,24 @@
+package com.halamoglu.ismekhomework
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>(){
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+}
